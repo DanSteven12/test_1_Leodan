2d51ebe74261d26b946d6ff1f5c687d455a5fede
DanSteven12 <hernandezhernandezcarlosleodan@gmail.com>
2026-02-13 06:44:54 -0600

    favicon

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" type="image/png" sizes="32x32" href="/CustomTkinter_icon_Windows.ico">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>holaa mundo</h1>
+    <button><a href="/acerca_de.php"> acerca de</a></button>
+</body>
+</html>
